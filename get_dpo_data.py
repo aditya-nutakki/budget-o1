eedae6d02f628fdfa3b14bbc5da037a088948d17
aditya-nutakki <adityanutakki6250@gmail.com>
2024-09-21 22:23:01 +0530

    get dpo data

--- get_dpo_data.py
+++ get_dpo_data.py
@@ -6,7 +6,8 @@ from config import *
 from modules import *
 from datasets import load_dataset
 import hashlib
-
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
 
 load_dotenv()
 
@@ -18,13 +19,7 @@ losing response would be whatever computed by your model
 
 These (prompt, winning_response, losing_response) should be done for both generating cot as well as the feedback
 
-
-1. Pass the question, ground truth and the cot generated from your model and ask the bigger model to generate the chain of thought which is along the lines of whatever was generated
-2. To generate the winning and losing responses for the feedback. You must pass the question, ground truth, newly generated cot and the wrong feedack to generate the correct feedback
-3. what about the part which incorporates feedback to  generated something (?)
-3. Similar to point2, take the generated cot/plan, give the newly generated feedback, the wrong cot from there to generate the correct COT
-
-4. The ultimate goal of this is to only refine the feedback, new idea, incorporation of new idea into the pipeline - figure it out !
+Then train another model with DPO.
 """
 
 
@@ -42,29 +37,33 @@ def update_data(data, prompt, chosen, rejected):
     data["prompt"].append(prompt)
     data["chosen"].append(chosen)
     data["rejected"].append(rejected)
-    # return data
 
 
 def get_dpo_data(tuner_model, student_model, split, level = "3", if_exists = "skip"):
-    dataset = load_my_data(split = split, level = level)
-    preference_data = {}
+
+
+    def _do_one_sample(sample):
+        preference_data = {}
+        if isinstance(sample, str):
+            sample = json.loads(sample)
     
-    save_dir = f"./beam_search/math/{tuner_model.model_name}/{level}_{split}/"
-    os.makedirs(save_dir, exist_ok = True)
-    
-    for i, sample in enumerate(dataset):
-        query, ground_truth, problem_type = sample["problem"], sample["solution"], sample["type"] # specific to this dataset; 
+        query, ground_truth, problem_type = sample["problem"], sample["ground_truth"], sample["problem_type"] # specific to this dataset; 
         _file_name = f"{hashlib.md5(str(sample).encode()).hexdigest()}.json"
         save_path = os.path.join(save_dir, _file_name)
         print(f"Going to save it to {save_path}")
+        sleep(1.5)
+        
         if os.path.exists(save_path):
             if if_exists == "skip":
-                continue
+                print(f"Skipping {save_path}")
+                return
 
         preference_data["prompt"] = []
         preference_data["chosen"] = []
         preference_data["rejected"] = []
-        preference_data["level"] = []
+        preference_data["level"] = level
+        preference_data["ground_truth"] = ground_truth
+        preference_data["query"] = query
         
         # try to get both the tuner as well as the student model to run together
 
@@ -101,22 +100,26 @@ def get_dpo_data(tuner_model, student_model, split, level = "3", if_exists = "sk
                 student_model_refinement = student_model.call(context = refine_context)
                 update_data(data = preference_data, prompt = refine_context[0]["content"], chosen = tuner_result["states"][i], rejected = student_model_refinement)
 
-                
-        
-        
-        """
-            eval:
-                1. 
-        """
 
         write_json(preference_data, filepath = save_path)
         print(f"done")
 
-        break
 
+    dataset = load_my_data(split = split, level = level)
+    
+    # save_dir = f"./beam_search/math/{tuner_model.model_name}/{level}_{split}/"
+    save_dir = f"./beam_search/math/dpo/{tuner_model.model_name}/{level}_{split}/"
+    os.makedirs(save_dir, exist_ok = True)
 
+    num_concurrent = 8
+    for i in range(0, len(dataset), num_concurrent):
+        samples = dataset[i : i + num_concurrent]
+        _samples = []
+        for problem, ground_truth, problem_type in zip(samples["problem"], samples["solution"], samples["type"]):
+            _samples.append({"problem": problem, "ground_truth": ground_truth, "problem_type": problem_type})
 
-
+        with ThreadPoolExecutor(max_workers = num_concurrent) as executor:
+            _ = list(executor.map(_do_one_sample, _samples))
 
 
 if __name__ == "__main__":
@@ -124,17 +127,15 @@ if __name__ == "__main__":
     save_dir = "./beam_search/"
 
     tuner_model = Model(client = "openai", model_name = "gpt-4o-mini")
-
-    # make sure your server on vLLM is running
+    # tuner_model = Model(client = "openai", model_name = "gpt-4o") # use this for harder problems - level 4 or level 5
+    # tuner_model = Model(client = "anthropic", model_name = "claude-3-5-sonnet-20240620") # use this for harder problems - level 4 or level 5
+    
+    # make sure your server via vLLM is running
     port = 8000
     base_url = f"http://localhost:{port}/v1"
     api_key = "token-abc123"
     student_model = Model(client = "openai", base_url = base_url, api_key = api_key, model_name = model_name)
     # print(tuner.call(context = [{"role":"user", "content": "hi how are you "}]))
     
-    get_dpo_data(tuner_model, student_model, split = "train")
-
-
-
-
+    get_dpo_data(tuner_model, student_model, split = "train", level = "2")
 
